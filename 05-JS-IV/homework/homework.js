0e791572c41446862506b38c4b69a0536d3f11c3
Niko <torresdnicolas@gmail.com>
2022-06-29 16:59:46 -0300

    js4 incompleto

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -91,6 +91,7 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  return usuario['password'] === password;
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
