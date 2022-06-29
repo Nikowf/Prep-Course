0a40fabfb180a42194db779a8709da0c53737984
Niko <torresdnicolas@gmail.com>
2022-06-29 18:44:27 -0300

    js4 incompleta

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -147,9 +147,9 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-  producto.calcularPrecioDescuento = function () {
-    return this.precio - (this.precio * this.porcentajededescuento);
-  }
+  producto.calcularPrecioDescuento = function() {
+    return this.precio - ( this.precio * this.porcentajeDeDescuento );
+  };
   return producto;
 }
 
